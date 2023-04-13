41bd551f60d6c0847ea89a6e8351c9e380456ea5
Roberto Guerra <prosaka@hotmail.com>
2023-04-13 20:01:25 -0300

    feat: Add Clarity script to _app.tsx
    This change adds the Clarity analytics script to the _app.tsx component using the dangerouslySetInnerHTML prop to inject the script into the html. Clarity can now track user behavior on the website.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,12 +15,26 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <NextSeo {...defaultSEOConfig} />
-
       <style jsx global>{`
         html {
           font-family: ${poppins.style.fontFamily};
         }
       `}</style>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+         (function(c,l,a,r,i,t,y){
+             c[a] = c[a] || function () { (c[a].q = c[a].q || 
+             []).push(arguments) };
+             t=l.createElement(r);
+             t.async=1;
+             t.src="https://www.clarity.ms/tag/"+i;
+             y=l.getElementsByTagName(r)[0];
+             y.parentNode.insertBefore(t,y);
+         })(window, document, "clarity", "script", "go4fu8cskt");`,
+        }}
+      />
+
       <Component {...pageProps} />
     </>
   );
