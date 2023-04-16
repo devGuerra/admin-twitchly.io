3a862a74ac1136c2eaac5bb982bd147e754b3121
Roberto Guerra <prosaka@hotmail.com>
2023-04-16 02:15:12 -0300

    teste render server site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
+
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       },
       colors: {
         red: {
-          600: "#E11138"
+          600: '#E11138'
         }
       },
       width: {
-        'max-container': '70rem',
+        'max-container': '70rem'
       }
-    },
+    }
   },
   plugins: [
-  ],
+  ]
 }
