c810af375cc2ce25a64f8e9625622e48ad3f8538
Roberto Guerra <prosaka@hotmail.com>
2023-06-21 21:56:15 -0300

    feat: new site twitchly

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,44 +2,43 @@ import { SealCheck } from "@phosphor-icons/react";
 import { NextSeo } from "next-seo";
 import Image from "next/image";
 
-import { Button } from "@/components/Button";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
-import { Alert } from "@/components/Alert";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <NextSeo title="Crie um catalago de filhotes com site para seu Canil" />
-      <main className="bg-white">
+      <NextSeo title="Twichly - Improve your twitch growth" />
+      <main className="">
         {/* Hero */}
         <section className="mt-16 mb-48 flex justify-center items-center container lg:w-max-container mx-auto px-4 lg:px-0">
           <div className="flex flex-col-reverse lg:flex-row justify-center gap-8">
-            <div className="flex flex-col flex-1 justify-center ">
+            <div className="flex flex-col flex-1 justify-center max-w-2xl">
               <h1 className="text-3xl lg:text-4xl font-extrabold text-center lg:text-left mb-2  ">
-                Você pode ter um site para seus{" "}
-                <span className="text-red-600">filhotes</span>
+                Improve your{" "}
+                <span className="text-purple-600">Twitch Channel</span>
               </h1>
-              <p className="text:md lg:text-lg text-center lg:text-left text-gray-600">
-                Tenha um catálogo com todos os seus filhotes e compartilhe seu
-                site com seus clientes. A melhor rede de criadores do Brasil.
+              <p className="text:md lg:text-lg text-center lg:text-left text-gray-400">
+                Twitchly is a platform that helps you to improve your channel
+                growth, with a lot of features that will help you to get more
+                followers and viewers.
               </p>
 
-              <Button type="button" className="lg:w-40 mt-2">
+              {/* <Button type="button" className="lg:w-40 mt-2">
                 Criar site
-              </Button>
+              </Button> */}
             </div>
             <div className="flex flex-col">
               <Image
                 src="/images/hero-phone.png"
-                width={600}
                 height={500}
-                alt="Imagem mostra um exemplo do site"
+                width={300}
+                alt="Example of the application"
                 quality={100}
                 unoptimized
-                className="rounded-md "
+                className="rounded-md self-center"
               />
             </div>
           </div>
@@ -49,8 +48,8 @@ export default function Home() {
           <div className="flex flex-col lg:flex-row justify-center gap-8">
             <div className="flex flex-col">
               <Image
-                src="/images/pet-page.png"
-                width={600}
+                src="/images/bonus-screen.png"
+                width={300}
                 height={500}
                 alt="Imagem mostra um exemplo do site"
                 quality={100}
@@ -58,90 +57,60 @@ export default function Home() {
                 className="rounded-md "
               />
             </div>
-            <div className="flex flex-col flex-1  ">
+            <div className="flex flex-col flex-1  max-w-2xl justify-center">
               <h2 className="text-3xl lg:text-4xl font-extrabold text-center lg:text-left mb-2  ">
-                Aqui você pode atender{" "}
-                <span className="text-red-600">mais clientes</span>
+                Get bonus <span className="text-purple-600">FREE</span>
               </h2>
               <div className="flex flex-row gap-2">
                 <SealCheck size={24} color="#27AE60" />
-                <p className="text:md lg:text-lg text-left text-gray-600">
-                  Você sempre online.
+                <p className="text:md lg:text-lg text-left text-gray-400">
+                  Get bonus for follow
                 </p>
               </div>
               <div className="flex flex-row gap-2">
                 <SealCheck size={24} color="#27AE60" />
-                <p className="text:md lg:text-lg text-left text-gray-600">
-                  Seu catalogo na palma da mão.
+                <p className="text:md lg:text-lg text-left text-gray-400">
+                  Get bonus for daily access.
                 </p>
               </div>
               <div className="flex flex-row gap-2">
                 <SealCheck size={24} color="#27AE60" />
-                <p className="text:md lg:text-lg text-left text-gray-600">
-                  Menos tempo com perguntas repetitivas.
-                </p>
-              </div>
-              <div className="flex flex-row gap-2">
-                <SealCheck size={24} color="#27AE60" />
-                <p className="text:md lg:text-lg text-left text-gray-600">
-                  Cadastro simples e rápido de seus filhotes.
-                </p>
-              </div>
-              <div className="flex flex-row gap-2">
-                <SealCheck size={24} color="#27AE60" />
-                <p className="text:md lg:text-lg text-left text-gray-600">
-                  Adicione a arvore genealógica de seus filhotes.
-                </p>
-              </div>
-              <div className="flex flex-row gap-2">
-                <SealCheck size={24} color="#27AE60" />
-                <p className="text:md lg:text-lg text-left text-gray-600">
-                  Compartilhe seu site com seus clientes.
+                <p className="text:md lg:text-lg text-left text-gray-400">
+                  Get bonus for refer a friend.
                 </p>
               </div>
             </div>
           </div>
         </section>
         {/* Site Examples */}
-        <section className="mb-32 flex justify-center items-center container lg:w-max-container mx-auto px-4 lg:px-0">
-          <div className="flex flex-col gap-8">
-            <h2 className="text-3xl lg:text-4xl font-extrabold text-center mb-2  ">
-              Seu site pronto em <span className="text-red-600">minutos</span>
-            </h2>
-            <p className="text:md lg:text-lg text-center  text-gray-600">
-              Seu catalogo de filhotes com fotos e descrições, compartilhe seu
-              site com seus clientes.
-            </p>
-            <div className="flex flex-col">
-              <Image
-                src="/images/site-example.png"
-                width={1120}
-                height={500}
-                alt="Imagem mostra um exemplo do site"
-                quality={100}
-                unoptimized
-                className="rounded-md opacity-70"
-              />
-            </div>
-          </div>
-        </section>
+
         <section className="mt-16 mb-48 flex justify-center items-center container lg:w-max-container mx-auto px-4 lg:px-0">
           <div className="flex flex-col-reverse lg:flex-row justify-center gap-8 lg:w-1/2">
             <div className="flex flex-col flex-1 justify-center ">
-              <h2 className="text-3xl lg:text-4xl font-extrabold text-center lg:text-left mb-2  ">
-                Comece agora mesmo e crie seu site em{" "}
-                <span className="text-red-600">minutos</span>
+              <h2 className="text-3xl lg:text-4xl font-extrabold text-center  mb-2  ">
+                <span className="text-purple-600 ">Download now</span>
               </h2>
-              <p className="text:md lg:text-lg text-center lg:text-left text-gray-600">
-                Você pode ter um site para seus filhotes, tenha um catálogo com
-                todos os seus filhotes e compartilhe seu site com seus clientes.
+              <p className="text:md lg:text-lg text-center lg:text-left text-gray-400">
+                Download now and start to improve your channel growth.
               </p>
-
-              <Link href="/dashboard">
-                <Button type="button" className=" mt-2">
-                  Criar site
-                </Button>
-              </Link>
+              <div className="flex justify-around mt-9 flex-col items-center gap-4s lg:flex-row">
+                <Link href="https://apps.apple.com/us/app/twitchly-increase-your-growth/id6450406278">
+                  <Image
+                    src="/images/apple-download.png"
+                    width={200}
+                    height={60}
+                    alt="download apple"
+                  />
+                </Link>
+                <Link href="https://play.google.com/store/apps/details?id=com.mobird.twitchly">
+                  <Image
+                    src="/images/android-download.png"
+                    width={200}
+                    height={60}
+                    alt="download apple"
+                  />
+                </Link>
+              </div>
             </div>
           </div>
         </section>
