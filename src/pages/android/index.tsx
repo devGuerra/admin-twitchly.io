7cf564640ac164d26b02a6989b9d0b4230dca341
Roberto Guerra <prosaka@hotmail.com>
2023-06-23 10:00:35 -0300

    create redirect

---
+++ src/pages/android/index.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import { SealCheck } from "@phosphor-icons/react";
+import { NextSeo } from "next-seo";
+
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
+
+export default function Android() {
+  useEffect(() => {
+    window.location.href =
+      "https://play.google.com/store/apps/details?id=com.mobird.twitchly";
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <NextSeo title="Twichly - Improve your twitch growth" />
+      <main className="">
+        {/* Hero */}
+        <section className="mt-16 mb-4 flex justify-center items-center container lg:w-max-container mx-auto px-4 lg:px-0">
+          <div className="flex flex-col-reverse lg:flex-row justify-center gap-8">
+            <div className="flex flex-col flex-1 justify-center ">
+              <h1 className=" font-extrabold text-center lg:text-left mb-2 mt-4  ">
+                Loading...
+              </h1>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
